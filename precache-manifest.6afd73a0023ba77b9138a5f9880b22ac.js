abb7bfaa7e757a82b221da10086cb8f3a1b5f692
Hyeeun <hyeeuni.mok@gmail.com>
2019-08-11 10:47:13 -0400

    Updates

---
+++ precache-manifest.6afd73a0023ba77b9138a5f9880b22ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b144aee6c666c10d0b0b757e2cc74373",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d2e8c64504120b7bbc8",
+    "url": "/static/css/main.d1f95185.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "9d2e8c64504120b7bbc8",
+    "url": "/static/js/main.76e712cd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
